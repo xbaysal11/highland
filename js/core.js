a7ed7c2d8754a690e7e638b20e1449cb421e6c37
alex <alexanderkim3125@gmail.com>
2019-08-05 07:38:59 +0600

    add country block

--- js/core.js
+++ js/core.js
@@ -1,3 +1,4 @@
+
 window.addEventListener('scroll', function(event) {
   var depth, i, layer, layers, len, movement, topDistance, translate3d;
   topDistance = this.pageYOffset;
@@ -13,24 +14,4 @@ window.addEventListener('scroll', function(event) {
     layer.style['-o-transform'] = translate3d;
     layer.style.transform = translate3d;
   }
-});
-
-var currentX = '';
-var currentY = '';
-var movementConstant = .015;
-$(document).mousemove(function(e) {
-  if(currentX == '') currentX = e.pageX;
-  var xdiff = e.pageX - currentX;
-  currentX = e.pageX;
-   if(currentY == '') currentY = e.pageY;
-  var ydiff = e.pageY - currentY;
-  currentY = e.pageY; 
-  $('.hc-parallax .test').each(function(i, el) {
-      var movement = (i + 1) * (xdiff * movementConstant);
-	  var movementy = (i + 1) * (ydiff * movementConstant);
-      var newX = $(el).position().left + movement;
-	  var newY = $(el).position().top + movementy;
-      $(el).css('left', newX + 'px');
-	  $(el).css('top', newY + 'px');
-  });
 });
