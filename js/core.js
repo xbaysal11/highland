c7e07d6bab219e1539f900ea87c63739e6c054b1
xbaysal11 <baisalbek.daniiarov@gmail.com>
2019-09-12 15:00:08 +0600

    MODIFIED fixed video section: autoplay modal video, play button hover

--- js/core.js
+++ js/core.js
@@ -201,3 +201,17 @@ $(document).scroll(function() {
     // $("#zorro2").removeClass("overlay2");
   }
 });
+
+$(function() {
+  $(".video").click(function() {
+    var theModal = $(this).data("target"),
+      videoSRC = $(this).attr("data-video"),
+      videoSRCauto =
+        videoSRC +
+        "?modestbranding=1&rel=0&controls=0&showinfo=0&html5=1&autoplay=1";
+    $(theModal + " iframe").attr("src", videoSRCauto);
+    $(theModal + " button.close").click(function() {
+      $(theModal + " iframe").attr("src", videoSRC);
+    });
+  });
+});
